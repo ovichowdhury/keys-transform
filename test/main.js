df87bb8bcb08390db8a0bc1d35bc442ef2a99cf3
Raju Ahmed <rajucse1705@gmail.com>
2021-10-19 16:05:54 +0600

    support array of objects

--- test/main.js
+++ test/main.js
@@ -8,28 +8,49 @@ const {
 
 function main() {
     const date = new Date();
-    const obj = {
-        NAME_ME: "Nahid Chowdhury",
-        AGE: 26,
-        hobby: ["Coding", "singing"],
-        job: {
-            professional: "engineer",
-            personal: null,
-        },
-        planet_Belongs: undefined,
-        nesT: {
-            dummy_me: {
-                hellO: "world",
+    const obj = [
+        {
+            NAME_ME: "Nahid Chowdhury",
+            AGE: 26,
+            hobby: ["Coding", "singing"],
+            job: {
+                professional: "engineer",
+                personal: null,
             },
+            planet_Belongs: undefined,
+            nesT: {
+                dummy_me: {
+                    hellO: "world",
+                },
+            },
+            date_test: date,
         },
-        date_test: date,
-    };
+        {
+            NAME_ME: "Md. Raju Ahmed",
+            AGE: 26,
+            hobby: ["Coding", "singing"],
+            job: {
+                professional: "engineer",
+                personal: null,
+            },
+            planet_Belongs: undefined,
+            nesT: {
+                dummy_me: {
+                    hellO: "world",
+                },
+            },
+            date_test: date,
+        },
+    ];
 
     console.log(toLowerKeys(obj));
 
     console.log(toUpperKeys(obj));
 
+    //array of object
     console.log(toCamelKeys(obj));
+    //object
+    console.log(toCamelKeys(obj[0]));
 
     console.log(toSnakeKeys(obj));
 
